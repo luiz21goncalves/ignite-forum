b6f7191e3a7ea0f4063be495b7806810b4ab8bba
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-05 20:56:40 -0300

    chore: add in memory repositories

---
+++ test/repositories/in-memory-answers-repository.ts
@@ -0,0 +1,14 @@
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+
+export class InMemoryAnswersRepository implements AnswersRepository {
+  private items: Answer[]
+
+  constructor() {
+    this.items = []
+  }
+
+  async create(answer: Answer): Promise<void> {
+    this.items.push(answer)
+  }
+}
