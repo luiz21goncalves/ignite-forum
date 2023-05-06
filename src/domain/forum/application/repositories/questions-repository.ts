07372aafe115d396d804787dc9e0a3096ec57a96
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-06 08:37:28 -0300

    chore: add method find by slug in questions-repository

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -2,4 +2,5 @@ import { Question } from '../../enterprise/entities/question'
 
 export type QuestionsRepository = {
   create(question: Question): Promise<void>
+  findBySlug(slug: string): Promise<Question | null>
 }
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -11,4 +11,16 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   async create(question: Question): Promise<void> {
     this.items.push(question)
   }
+
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = this.items.find(
+      (findQuestion) => findQuestion.slug.value === slug,
+    )
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
 }
