f20a2720c31472d76c1d425e99bb23b57c5b63b4
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-06 08:35:20 -0300

    chore: add create method to slug value object

--- src/domain/forum/enterprise/entities/value-objects/slug.ts
+++ src/domain/forum/enterprise/entities/value-objects/slug.ts
@@ -1,8 +1,12 @@
 export class Slug {
   public readonly value: string
 
-  constructor(value: string) {
+  protected constructor(value: string) {
     this.value = value
+  }
+
+  public static create(value: string) {
+    return new Slug(value)
   }
 
   /**
