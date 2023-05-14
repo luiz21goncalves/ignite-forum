86bcf6e86462c4f710641753351168a69f6f2e15
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-14 09:21:06 -0300

    chore: add edit answer repository

---
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,56 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeAnswer } from '@test/factories/make-answer'
+import { InMemoryAnswersRepository } from '@test/repositories/in-memory-answers-repository'
+
+import { EditAnswerUseCase } from './edit-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: EditAnswerUseCase
+
+describe('Edit Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to edit a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    const { answer } = await sut.execute({
+      answerId: 'answer-1',
+      authorId: 'author-1',
+      content: 'Conteúdo test',
+    })
+
+    expect(answer).toMatchObject({ content: 'Conteúdo test' })
+  })
+
+  it('should not be able to edit a answer from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await expect(() =>
+      sut.execute({
+        answerId: 'answer-1',
+        authorId: 'author-2',
+        content: 'Conteúdo test',
+      }),
+    ).rejects.toStrictEqual(new Error('Not allowed.'))
+  })
+})
---
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -0,0 +1,38 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+type EditAnswerUseCaseRequest = {
+  answerId: string
+  authorId: string
+  content: string
+}
+
+type EditAnswerUseCaseResponse = {
+  answer: Answer
+}
+
+export class EditAnswerUseCase {
+  constructor(private readonly answersRepository: AnswersRepository) {}
+
+  public async execute({
+    answerId,
+    authorId,
+    content,
+  }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    answer.content = content
+
+    await this.answersRepository.save(answer)
+
+    return { answer }
+  }
+}
