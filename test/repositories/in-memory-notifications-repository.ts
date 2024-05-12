cc4b746dc0d4c31b6293d12b989c00d156015fc0
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-12 10:03:36 -0300

    chore: add read notification use-case

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -10,7 +10,25 @@ export class InMemoryNotificationsRepository
     this.items = []
   }
 
+  async findById(id: string): Promise<Notification | null> {
+    const notification = this.items.find((item) => item.id.toString() === id)
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
+
   async create(notification: Notification): Promise<void> {
     this.items.push(notification)
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id.toString() === notification.id.toString(),
+    )
+
+    this.items[itemIndex] = notification
   }
 }
